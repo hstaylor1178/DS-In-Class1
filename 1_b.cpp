8a28c13153d59ffeb7adc8c31929161fbb96a17b
hstaylor1178 <105338054+hstaylor1178@users.noreply.github.com>
2024-09-10 19:18:22 -0500

    Update 1_b.cpp

--- 1_b.cpp
+++ 1_b.cpp
@@ -10,7 +10,29 @@ int main()
         *myPtr = 5;
         yourPtr = myPtr;
         cout << *yourPtr << endl;
-        //delete yourPtr;
         cout << *myPtr << endl;
-          
+		
+		delete yourPtr; // don't delete the pointer before you're done using it
+		 
+		return 0; 
 }
+
+/*
+ Original:
+	#include<iostream>
+	#include<iomanip>
+	 
+	using namespace std;
+	 
+	int main()
+	{
+		int* myPtr = new int;
+			int* yourPtr;
+			*myPtr = 5;
+			yourPtr = myPtr;
+			cout << *yourPtr << endl;
+			delete yourPtr;
+			cout << *myPtr << endl;
+			  
+	}
+*/
